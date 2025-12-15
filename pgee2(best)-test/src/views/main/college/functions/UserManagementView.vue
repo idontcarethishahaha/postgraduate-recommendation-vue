213001c2863ccb9e1a31b8b1ba6aa03d5d3e7290
wuwenjincat <tomatocat1130@qq.com>
2025-12-15 16:14:39 +0800

    2025.12.15 提交修改

--- pgee2(best)-test/src/views/main/college/functions/UserManagementView.vue
+++ pgee2(best)-test/src/views/main/college/functions/UserManagementView.vue
@@ -2,11 +2,10 @@
 import { createElNotificationSuccess } from '@/components/message'
 import { CollegeService } from '@/services/CollegeService'
 import type { RegisterUserDTO } from '@/types'
-import { User as UserICO } from '@element-plus/icons-vue'
 import { computed, ref, shallowRef } from 'vue'
+
 // 添加辅导员
 const { data: result, suspense } = CollegeService.listCounselorsService()
-
 await suspense()
 const categoriesR = shallowRef((result.value ?? []).map(res => res.category))
 
@@ -22,15 +21,18 @@ const submitF = async () => {
   userR.value = {}
   catidsR.value = []
 }
+
 const submitC = computed(() => {
   return userR.value.name && userR.value.account && catidsR.value?.length > 0
 })
 </script>
+
 <template>
   <el-row class="my-row">
     <el-col>
       <el-form label-width="80px" style="width: 400px">
         <el-form-item>
+          <p class="info">再此添加辅导员(默认密码为工号)</p>
           <p v-for="catUser of result" :key="catUser.category?.id">
             {{ catUser.category?.name }}:
             <span v-for="user of catUser.users" :key="user.id">{{ user.name }};</span>
@@ -54,11 +56,42 @@ const submitC = computed(() => {
           </el-checkbox-group>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitF" :disabled="!submitC">
-            <el-icon><UserICO /></el-icon>
+          <el-button
+            @click="submitF"
+            :disabled="!submitC"
+            style="padding: 8px 16px; border-radius: 4px">
+            添加辅导员
           </el-button>
         </el-form-item>
       </el-form>
     </el-col>
   </el-row>
 </template>
+
+<style scoped>
+.info {
+  color: #769ff1;
+  font-size: 15px;
+  margin-bottom: 10px;
+}
+
+:deep(.el-button) {
+  color: #ffffff !important;
+  border-color: #1890ff !important;
+  background-color: #1890ff !important;
+  border: none;
+}
+
+:deep(.el-button:disabled) {
+  background-color: #a0cfff !important;
+  border-color: #a0cfff !important;
+  color: #ffffff !important;
+  cursor: not-allowed;
+}
+
+:deep(.el-button:not(:disabled):hover) {
+  background-color: #40a9ff !important;
+  border-color: #40a9ff !important;
+  color: #ffffff !important;
+}
+</style>
